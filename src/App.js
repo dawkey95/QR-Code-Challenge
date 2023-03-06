d538a74a644cfc1faedcd47e0962f411ff4ac7a1
dawkey95 <dawidkeyser95@gmail.com>
2023-03-07 12:03:31 +1300

    html and favicon updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+
+
 function App() {
-	return <div className="App">App Goes here</div>;
+	return (
+		<div className="App">
+			<p>Improve your front-end skills by building projects Scan the QR code to visit</p>
+			<p>Frontend Mentor and take your coding skills to the next level</p>
+
+			<div class="attribution">
+				Challenge by{' '}
+				<a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+					Frontend Mentor
+				</a>
+				. Coded by <a href="#">Dawid Keyser</a>.
+			</div>
+		</div>
+	);
 }
 
 export default App;
